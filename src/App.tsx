9abc82b66d093213b789e057c6a842a2e8795bb4
Bill ON <bill.on@epitech.eu>
2024-06-14 21:32:47 +0800

    [UPDATE] site api

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,107 @@
 import React from 'react';
 import './App.css';
-import { sendAudio } from './sendAudio';
-import { useFilePicker } from 'use-file-picker';
+import { FileUploader } from "react-drag-drop-files";
+import { checkStatus, sendAudio, getFileData } from './sendAudio';
+
+const saveAsTextFile = (data: string, filename: string) => {
+  const blob = new Blob([data], { type: 'text/plain;charset=utf-8' });
+  const link = document.createElement('a');
+
+  link.href = URL.createObjectURL(blob);
+  link.download = `${filename}.txt`;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
 
 export default function App() {
+  interface ButtonProps {
+    color: string;
+    text: string;
+    onClick: any;
+  }
+  //flac, mp3, mp4, mpeg, mpga, m4a, ogg, wav, or webm.
+  const fileTypes = ["flac", "mp4", "mp3", "mpeg", "m4a", "ogg", "wav", "webm"];
+  const [file, setFile] = React.useState<File | null>(null);
+  const [jobId, setJobID] = React.useState("");
+  const [status, setStatus] = React.useState<Map<string, string>>(new Map());
 
-  const LoadingScreen = () => {
+  const accessToken = "02d9*";
+
+  function DragDrop() {
+
+    const handleChange = (file: File) => {
+      setFile(file);
+      const fileUri = URL.createObjectURL(file);
+      alert(`FILE CHARGED: ${fileUri}`);
+    };
     return (
-      <div>
-        Loading...
-      </div>
+      <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
     );
   }
-  const FileSelector = () => {
-    return (
-      <div>
-        <button onClick={() => openFilePicker()} disabled={loading}>Select file</button>
-        {filesContent.map((file, index) => (
-          <div key={index}>
-            <h2>{file.name}</h2>
-            <br />
-          </div>
-        ))}
-      </div>
-    );
+
+  const transmitData = async (file: File) => {
+    const res = await sendAudio(file, accessToken)
+    if (res != null) {
+      alert(`JOB ID BOZZO: ${res.id}`);
+      setJobID(res.id);
+      setStatus(prevMap => {
+        const newMap = new Map(prevMap);
+        newMap.set(res.id, res.status);
+        return newMap;
+      });
+    }
+
+    const stat = await checkStatus(jobId, accessToken)
+    alert(`CHECK DATA STATUS: ${JSON.stringify(stat)}`);
+    console.log(`CHECK DATA STATUS: ${JSON.stringify(stat)}`);
   }
 
-  const { openFilePicker, filesContent, loading } = useFilePicker({
-    accept: '.pdf',
-  });
-
+  const Button: React.FC<ButtonProps> = ({ color, text, onClick }) => {
+    return (
+      <button
+        style={{
+          backgroundColor: color,
+          color: 'white',
+          fontWeight: 'bold',
+          padding: '10px 20px',
+          borderRadius: '10px',
+          border: 'none',
+          cursor: 'pointer',
+        }}
+        onClick={onClick}
+      >
+        {text}
+      </button>
+    );
+  };
 
   return (
-    <div className="App">
-      {loading ? <LoadingScreen /> : <FileSelector />}
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+      <h1 style={{ textAlign: 'center', marginTop: '5%', fontSize: '3em' }}>
+        Software Engineering Final Project
+      </h1>
+      <div style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'flex-start' }}>
+        <DragDrop />
+      </div>
+
+      {(file == null) ? null :
+        <div style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+          <h2>{file.name}</h2>
+          <div >
+            <Button color="blue" text="Download Subtitles" onClick={() => { transmitData(file) }} />
+          </div>
+        </div>}
+        <Button color="pink" text="check status" onClick={async () => {
+        const stat = await checkStatus(jobId, accessToken)
+        alert(`CHECK DATA STATUS: ${JSON.stringify(stat)}`);
+        console.log(`CHECK DATA STATUS: ${JSON.stringify(stat)}`);
+      }} />
+      <Button color="cyan" text="Retrieve File" onClick={async () => {
+        const stat = await getFileData(jobId, accessToken)
+        alert(`GET DATA STATUS: ${JSON.stringify(stat)}`);
+        console.log(`GET DATA STATUS: ${JSON.stringify(stat)}`);
+      }} />
     </div>
   );
 }
--- src/sendAudio.tsx
+++ src/sendAudio.tsx
@@ -1,20 +1,80 @@
-export async function sendAudio(file: any) {
+export async function sendAudio(file: File, token: string) {
   try {
-    fetch("", {
+    // curl -X POST "https://localhost:3000/poyo" 
+    //   -H "Authorization: Bearer $ACCESS_TOKEN"
+    //   -H "Content-Type: multipart/form-data" 
+    //   -F "media=@/path/to/media_file.mp3;type=audio/mp3"
+    //   -F "options={\"metadata\":\"This is a sample submit jobs option for multipart\", \"custom_vocabularies\":[{\"phrases\": [\"Amelia Earhart\", \"Paul McCartney\"]}]}"
+
+    const formData = new FormData();
+    formData.append('media', file, file.name);
+    formData.append('options', JSON.stringify({
+      metadata: 'This is a sample submit jobs option for multipart',
+      custom_vocabularies: [
+        { phrases: ['Amelia Earhart', 'Paul McCartney'] }
+      ]
+    }));
+    const request = await fetch("https://api.rev.ai/speechtotext/v1/jobs", {
       method: "POST",
       headers: {
-        'content-type': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
       },
-      body: JSON.stringify(file)
-    }).then(async (response) => {
-      const response_body = await response.json();
-      if (response.status < 200 && response.status > 399) {
-        throw new Error(`Request Error: ${JSON.stringify(response_body)}`);
-      } else {
-        return response_body;
-      }
+      body: formData
     });
+    console.log("SEND AUD: ", request.status);
+    const request_body = await request.json();
+    if (request.status < 200 && request.status > 399) {
+      throw new Error(`Request Error: ${JSON.stringify(request_body)}`);
+    } else {
+      return request_body;
+    }
   } catch (error) {
     alert(`Error while sending file : ${JSON.stringify(error)}`);
+  }
+}
+
+export async function checkStatus(id: string, token: string) {
+  try {
+    console.log(`CHECK STAT ID : ${id} - tok: ${token}`);
+    const request = await fetch(`https://api.rev.ai/speechtotext/v1/jobs/${id}`, {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+    });
+    console.log("FETCH OVERTT");
+    const request_body = await request.json();
+    if (request.status < 200 && request.status > 399) {
+      alert(`${request.status} - Request Error: ${JSON.stringify(request_body)}`)
+      throw new Error(`${request.status} - Request Error: ${JSON.stringify(request_body)}`);
+    } else {
+      return request_body;
+    }
+  } catch (e) {
+    alert(`Error while Checking Status : ${JSON.stringify(e)}`);
+  }
+}
+
+export async function getFileData(id: string, token: string) {
+  try {
+    const request = await fetch(`https://api.rev.ai/speechtotext/v1/jobs/${id}/transcript`, {
+      method: "GET",
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Accept': 'application/vnd.rev.transcript.v1.0+json'
+      },
+    });
+    const request_body = await request.json();
+    if (request.status < 200 && request.status > 399) {
+      alert(`${request.status} - Request Error: ${JSON.stringify(request_body)}`)
+      throw new Error(`${request.status} - Request Error: ${JSON.stringify(request_body)}`);
+    } else {
+      return request_body;
+    }
+
+  } catch (e) {
+    alert(`Error While Retrieving data : ${JSON.stringify(e)}`);
   }
 }
